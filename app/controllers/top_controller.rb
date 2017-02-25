6531433af8588091923be8913d23f45e1109c9ad
shoehead0125 <shoehead0125@gmail.com>
2017-02-25 19:36:34 +0900

    Revised top_controller

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -7,6 +7,6 @@ class TopController < ApplicationController
 
   private
   def move_to_sign_in
-    redirect_to '/users/sign_in' unless user_signed_in?
+    redirect_to new_user_session_path unless user_signed_in?
   end
 end
