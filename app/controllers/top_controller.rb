5e58cb9c5b69ab6ae7ed14dc6686b6af9f4e1d0b
shoehead0125 <shoehead0125@gmail.com>
2017-02-25 18:07:14 +0900

    Make before action

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,12 @@
 class TopController < ApplicationController
 
+  before_action :move_to_sign_in
+
   def index
   end
 
+  private
+  def move_to_sign_in
+    redirect_to '/users/sign_in' unless user_signed_in?
+  end
 end
