5207dfb31280fc30b930d236b6329a2a66faeb1e
shoehead0125 <shoehead0125@gmail.com>
2017-02-26 18:31:09 +0900

    Revised model.rb
    アソシエーションのための記述が不足していたので、修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :groups, :through => :user_groups
+  has_many :groups, through: :user_groups
+  has_many :user_groups
 end
