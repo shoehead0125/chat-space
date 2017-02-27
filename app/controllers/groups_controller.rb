ecaf8b98068d983afbab511a9a239ac24a5fd0b3
shoehead0125 <shoehead0125@gmail.com>
2017-02-27 21:58:35 +0900

    Edit view file(group/new)
    formタグからヘルパー form forへ書き直し

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,15 +1,17 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new(params[:group])
   end
 
   def create
+    binding.pry
     Group.create(group_params)
   end
 
   private
   def group_params
-    params.permit(:string, :name)
+    params.require(:group).permit(:name)
   end
 
 end
