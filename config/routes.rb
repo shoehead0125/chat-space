f79a2ee3257e05847f99fec0ea7ed672b21fc26b
shoehead0125 <shoehead0125@gmail.com>
2017-02-28 16:25:26 +0900

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
 
-  resources :groups, except: [:show,:index, :destroy]
-
+  resources :groups, except: [:show,:index, :destroy] do
+    resources :messages, only: [:index]
+  end
 end
