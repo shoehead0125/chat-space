d8961eccd6c8b002f61a7b7114b8b0ca00cb902b
shoehead0125 <shoehead0125@gmail.com>
2017-03-03 15:15:22 +0900

    Edit messages_controller
    - 共通でないインスタンスの生成の削除
    - 場合に応じたエラーメッセージの出力
    - N+1問題の回避

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,29 +1,29 @@
 class MessagesController < ApplicationController
   before_action :store_messages_instance
 
-  def index; end
+  def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
+  end
 
   def create
     @message = Message.new(message_params)
     if @message.save
       redirect_to group_messages_path, notice: 'メッセージが投稿されました。'
     else
-      flash.now[:alert] = '空のメッセージです。'
+      flash.now[:alert] = @message.errors.full_messages.first
       render :index
     end
   end
 
   private
   def message_params
-    params[:user_id] = current_user.id
-    params.permit(:body, :image, :group_id, :user_id)
+    params.require(:message).permit(:body).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 
   def store_messages_instance
-    @group = Group.find(params[:group_id])
+    @group = Group.includes(:messages).find(params[:group_id])
     @groups = current_user.groups
-    @users = @group.users
-    @messages = @group.messages
   end
 
 end
