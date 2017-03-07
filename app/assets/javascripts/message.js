661fdb70544fb5903f547a56c5f746474da34961
shoehead0125 <shoehead0125@gmail.com>
2017-03-07 18:02:35 +0900

    修正
    - デバックコードの削除
    - 式展開の簡素化

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,8 @@
 $(function(){
   function buildHTML(message){
-    var tweet_name = (`<p class=tweet--name>${message.message.name}`);
-    var tweet_time = (`<p class=tweet--time>${message.message.created_at}`);
-    var tweet      = (`<p class=tweet>${message.message.body}`);
+    var tweet_name = (`<p class=tweet--name>${ message.name }`);
+    var tweet_time = (`<p class=tweet--time>${ message.created_at }`);
+    var tweet      = (`<p class=tweet>${ message.body }`);
     var html = $('<div class="chat-space__tweet">').append(tweet_name, tweet_time, tweet);
     return html;
   }
@@ -22,13 +22,11 @@ $(function(){
       dataType: 'json'
     })
     .done(function(data){
-      var html = buildHTML(data);
-      console.log(data);
+      var html = buildHTML(data.message);
       $('.right-contents__chat-space').append(html);
       textField.val('');
     })
     .fail(function(){
-      debugger;
       alert('error');
     });
   });
