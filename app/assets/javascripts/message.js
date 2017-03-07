f294e098329c36e39f3ea215e5ab7d3e2959e757
shoehead0125 <shoehead0125@gmail.com>
2017-03-07 17:41:00 +0900

    jsの式展開の適用

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,8 @@
 $(function(){
   function buildHTML(message){
-    var tweet_name = $("<p class=tweet__name>#{message.user.name}");
-    var tweet_time = $("<p class=tweet__time>#{message.created_at.strftime('%Y年%m月%d日 %H:%M:%S')}");
-    var tweet       = $("<p class=tweet>#{message.body}")
+    var tweet_name = (`<p class=tweet--name>${message.message.name}`);
+    var tweet_time = (`<p class=tweet--time>${message.message.created_at}`);
+    var tweet      = (`<p class=tweet>${message.message.body}`);
     var html = $('<div class="chat-space__tweet">').append(tweet_name, tweet_time, tweet);
     return html;
   }
@@ -23,10 +23,12 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
+      console.log(data);
       $('.right-contents__chat-space').append(html);
       textField.val('');
     })
     .fail(function(){
+      debugger;
       alert('error');
     });
   });
