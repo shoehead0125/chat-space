404e4c6e953f13e6a0c3b05ed5bbdd42e8c516b3
shoehead0125 <shoehead0125@gmail.com>
2017-03-08 18:43:24 +0900

    Search user in user controller
    コントローラにて検索をして、jbuilderに検索結果を渡す

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+
+  def index
+    reg = params[:search_character]
+    @users = User.where("name like '#{reg}'").or(User.where("kana like '#{reg}'"))
+    render 'search_results', formats: [:json], handlers: [:jbuilder]
+  end
 end
