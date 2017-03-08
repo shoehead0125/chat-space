0fa0065da6bc0ecb7463853043c43c5d41a3e6ba
shoehead0125 <shoehead0125@gmail.com>
2017-03-08 20:08:45 +0900

    Add scope for searching

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
 
   def index
     reg = params[:search_character]
-    @users = User.where("name like '#{reg}'").or(User.where("kana like '#{reg}'"))
+    binding.pry
+    @users = User.including_name_or_kana(reg)
     render 'search_results', formats: [:json], handlers: [:jbuilder]
   end
 end
