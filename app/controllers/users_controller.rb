db2f3dce4123107b4953426c2348b73e1a0e0460
shoehead0125 <shoehead0125@gmail.com>
2017-03-08 20:12:05 +0900

    いらない行の削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
 
   def index
     reg = params[:search_character]
-    binding.pry
     @users = User.including_name_or_kana(reg)
     render 'search_results', formats: [:json], handlers: [:jbuilder]
   end
